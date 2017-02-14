93b840354fca7aca0a61c2fa627afdc8190f7f10
semistone222@gmail.com <semistone222@gmail.com>
2017-02-14 14:02:06 +0900

    다른 인트로 라이브러리 시도

--- app/src/main/java/com/semistone/donately/intro/IntroActivity.java
+++ app/src/main/java/com/semistone/donately/intro/IntroActivity.java
@@ -1,76 +1,42 @@
 package com.semistone.donately.intro;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
+
+import android.graphics.Color;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.View;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 
-import com.chyrta.onboarder.OnboarderActivity;
-import com.chyrta.onboarder.OnboarderPage;
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
 import com.semistone.donately.R;
-import com.semistone.donately.login.LoginActivity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class IntroActivity extends OnboarderActivity {
-
-    private List<OnboarderPage> onboarderPages;
+public class IntroActivity extends AppIntro {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        onboarderPages = new ArrayList<OnboarderPage>();
 
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-        boolean isNotFirst = sharedPref.getBoolean(getString(R.string.pref_intro_not_first_key), getResources().getBoolean(R.bool.pref_intro_not_first_default));
-        if (isNotFirst) {
-            FinishIntro();
-        } else {
-            SharedPreferences.Editor editor = sharedPref.edit();
-            editor.putBoolean(getString(R.string.pref_intro_not_first_key), true);
-            editor.commit();
-        }
+        addSlide(AppIntroFragment.newInstance(getString(R.string.onboaderpage1_title), getString(R.string.onboaderpage1_description), R.drawable.ic_loving, ContextCompat.getColor(this, R.color.colorAccent)));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.onboaderpage2_title), getString(R.string.onboaderpage2_description), R.drawable.ic_video_play, ContextCompat.getColor(this, R.color.colorAccent)));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.onboaderpage3_title), getString(R.string.onboaderpage3_description), R.drawable.ic_free, ContextCompat.getColor(this, R.color.colorAccent)));
+        addSlide(SampleSlide.newInstance(R.layout.intro_login));
 
-        OnboarderPage onboarderPage1 = new OnboarderPage(R.string.onboaderpage1_title, R.string.onboaderpage1_description, R.drawable.ic_loving);
-        OnboarderPage onboarderPage2 = new OnboarderPage(R.string.onboaderpage2_title, R.string.onboaderpage2_description, R.drawable.ic_video_play);
-        OnboarderPage onboarderPage3 = new OnboarderPage(R.string.onboaderpage3_title, R.string.onboaderpage3_description, R.drawable.ic_free);
-
-        onboarderPage1.setTitleColor(R.color.white);
-        onboarderPage1.setDescriptionColor(R.color.white);
-        onboarderPage1.setBackgroundColor(R.color.colorPrimaryDark);
-
-        onboarderPage2.setTitleColor(R.color.white);
-        onboarderPage2.setDescriptionColor(R.color.white);
-        onboarderPage2.setBackgroundColor(R.color.colorPrimary);
-
-        onboarderPage3.setTitleColor(R.color.white);
-        onboarderPage3.setDescriptionColor(R.color.white);
-        onboarderPage3.setBackgroundColor(R.color.colorPrimaryDark);
-
-        onboarderPages.add(onboarderPage1);
-        onboarderPages.add(onboarderPage2);
-        onboarderPages.add(onboarderPage3);
-
-        setOnboardPagesReady(onboarderPages);
-
-        setDividerVisibility(View.GONE);
-        shouldUseFloatingActionButton(true);
+        showSkipButton(false);
     }
 
     @Override
-    public void onSkipButtonPressed() {
-        super.onSkipButtonPressed();
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
     }
 
     @Override
-    public void onFinishButtonPressed() {
-        FinishIntro();
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
     }
 
-    private void FinishIntro() {
-        startActivity(new Intent(IntroActivity.this, LoginActivity.class));
-        finish();
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
     }
 }

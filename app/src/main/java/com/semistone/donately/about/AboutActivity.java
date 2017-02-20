fd040e9f3ffabc486fb09f12c4879b4abbed3be1
semistone222@gmail.com <semistone222@gmail.com>
2017-02-20 13:33:34 +0900

    #34 about 액티비티 작업 中

--- app/src/main/java/com/semistone/donately/about/AboutActivity.java
+++ app/src/main/java/com/semistone/donately/about/AboutActivity.java
@@ -1,7 +1,10 @@
 package com.semistone.donately.about;
 
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.semistone.donately.R;
 
@@ -11,5 +14,20 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
